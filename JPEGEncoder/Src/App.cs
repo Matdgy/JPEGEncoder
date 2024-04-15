f1d48d4d19b68a3b7ed5c221c592003ad8c54e9d
Máthé Dávid György <matdgy@gmail.com>
2024-04-15 19:10:26 +0200

    added gitignore, directory build props

---
+++ JPEGEncoder/Src/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEGEncoder
+{
+	internal class App
+	{
+		static public void Main(string[] args) {
+			Console.WriteLine("Entry");
+		}
+	}
+}
