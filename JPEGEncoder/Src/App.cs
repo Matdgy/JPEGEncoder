491b740b3f99f091a8b25131c43f560224e16aee
Máthé Dávid György <matdgy@gmail.com>
2024-04-15 21:43:18 +0200

    added int conversion, padding, sample bitmap

--- JPEGEncoder/Src/App.cs
+++ JPEGEncoder/Src/App.cs
@@ -4,13 +4,26 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace JPEGEncoder
 {
-	internal class App
-	{
+	internal class App {
 		static public void Main(string[] args) {
-			Console.WriteLine("Entry");
+			foreach (string arg in args) {
+
+				// Read file
+				Bitmap bitmap = new Bitmap(arg);
+
+				// Convert the bitmap to a 2d integer array
+				// This function also converts the ARGB values to greyscale
+				int[,] img = Utils.ConvertBitmapToInt(bitmap);
+
+				// DCT requires an image which has its width and height divisible by 8
+				// Extend the image by repeating the last pixels
+				img = Utils.AddPadding(img);
+
+			}
 		}
 	}
 }
---
+++ JPEGEncoder/Src/Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEGEncoder
+{
+	internal class Utils {
+		public static int[,] ConvertBitmapToInt(Bitmap bitmap) {
+			
+			int[,] img = new int[bitmap.Width, bitmap.Height];
+
+			for (int i = 0; i < bitmap.Width; i++) {
+				for (int j = 0; j < bitmap.Height; j++) {
+
+					Color act = bitmap.GetPixel(i, j);
+					double greyscaleValue = act.R * 0.2989 + act.G * 0.5870 + act.B * 0.1140;
+
+					img[i,j] = Convert.ToInt32(greyscaleValue) < 255 ? Convert.ToInt32(greyscaleValue) : 255;
+
+				}
+			}
+			return img;
+		}
+
+		public static int[,] AddPadding(int[,] img) {
+
+			int addX = (8 - img.GetLength(0) % 8) < 8 ? 8 - img.GetLength(0) % 8 : 0;
+			int addY = (8 - img.GetLength(1) % 8) < 8 ? 8 - img.GetLength(1) % 8 : 0;
+
+			int[,] result = new int[img.GetLength(0) + addX, img.GetLength(1) + addY];
+
+			if (addX != 0 || addY != 0) {
+
+				for (int i = 0; i < img.GetLength(0); i++) {
+					for (int j = 0; j < img.GetLength(1); j++) {
+						result[i, j] = img[i, j];
+					}
+				}
+
+				for (int j = 0; j < img.GetLength(1); j++) {
+					for (int i = 1; i <= addX; i++) {
+						result[img.GetLength(0) - 1 + i, j] = result[img.GetLength(0) - 1 + i - 1, j];
+					}
+				}
+
+				for (int j = 0; j < result.GetLength(0); j++) {
+					for (int i = 1; i <= addY; i++) {
+						result[j, img.GetLength(1) - 1 + i] = result[j, img.GetLength(1) - 1 + i - 1];
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
